41857ef137f2e56d31556fbf9a262a06d5cbede9
Sourciluss667 <qcormand@sourciluss.net>
2019-05-21 15:20:20 +0200

    add title and clients.php

---
+++ dev/views/clients.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>EasyBusiness - Clients</title>
+    <link rel="stylesheet" href="public/css/main.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+</head>
+<body>
+
+<?php require('template/top.php'); ?>
+<?php require('template/navbar.php'); ?>
+
+<!-- TOUT ICI -->
+
+
+
+
+<!-- FIN TOUT ICI -->
+
+<?php require('template/bottom.php'); ?>
+
+<script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+<script src="public/js/vue.js"></script>
+<script>
+const app = new Vue({
+    el: '#app',
+    data: {}
+})
+</script>
+</body>
+</html>
